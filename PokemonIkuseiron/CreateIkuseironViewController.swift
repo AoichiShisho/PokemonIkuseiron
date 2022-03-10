5612acaf30d61daf19a28254faa335d012532e41
So Nakamura <sonakamura.aoichi@gmail.com>
2022-03-10 18:05:20 +0900

    form implemented

--- PokemonIkuseiron/CreateIkuseironViewController.swift
+++ PokemonIkuseiron/CreateIkuseironViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Eureka
 
-class CreateIkuseironViewController: UIViewController, UINavigationBarDelegate {
+class CreateIkuseironViewController: FormViewController, UINavigationBarDelegate {
     @IBOutlet weak var createIkuseironNavigationBar: UINavigationBar!
 
     override func viewDidLoad() {
@@ -16,6 +16,53 @@ class CreateIkuseironViewController: UIViewController, UINavigationBarDelegate {
 
         // NavigationBarの設定
         createIkuseironNavigationBar.delegate = self
+        
+        form
+            +++ Section("基本情報")
+            <<< NameRow("name") {
+                $0.title = "ポケモン名"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("seikaku") {
+                $0.title = "性格"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("tokusei") {
+                $0.title = "特性"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("doryokuchi") {
+                $0.title = "努力値配分"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("kotaichi") {
+                $0.title = "理想個体値"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("item") {
+                $0.title = "持ち物"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("moves") {
+                $0.title = "覚えさせる技"
+                $0.add(rule: RuleRequired())
+            }
+        
+            +++ Section("対戦形式")
+            <<< PickerInputRow<String>("rule") {
+                $0.title = "対戦形式・ルール"
+                $0.options = ["シングルバトル", "ダブルバトル"]
+        }
+        
+            +++ Section("育成論の考察")
+            <<< NameRow("title") {
+                $0.title = "タイトル"
+                $0.add(rule: RuleRequired())
+            }
+            <<< NameRow("kousatu") {
+                $0.title = "考察"
+                $0.add(rule: RuleRequired())
+            }
     }
     
     // これによってNavigationBarを上につなげてる
