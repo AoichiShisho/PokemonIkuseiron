2ab6120d71bb809924982731a90c223777006035
So Nakamura <sonakamura.aoichi@gmail.com>
2022-03-20 18:57:09 +0900

    DetailView Controllerのデータ出力画面　途中経過

--- PokemonIkuseiron/DetailViewController.swift
+++ PokemonIkuseiron/DetailViewController.swift
@@ -12,6 +12,18 @@ class DetailViewController: UIViewController, UINavigationBarDelegate {
 
     @IBOutlet weak var detailViewNavigationBar: UINavigationBar!
     @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var seikakuText: UILabel!
+    @IBOutlet weak var tokuseiText: UILabel!
+    @IBOutlet weak var doryokuchiText: UILabel!
+    @IBOutlet weak var movesText: UILabel!
+    @IBOutlet weak var itemText: UILabel!
+    @IBOutlet weak var ruleText: UILabel!
+    @IBOutlet weak var kousatuText: UILabel!
+    
+    @IBOutlet weak var seikakuView: UIView!
+    @IBOutlet weak var tokuseiView: UIView!
+    @IBOutlet weak var doryokuchiView: UIView!
     
     var ikuseiron: Ikuseiron!
     
@@ -20,11 +32,29 @@ class DetailViewController: UIViewController, UINavigationBarDelegate {
 
         // NavigationBarの設定
         detailViewNavigationBar.delegate = self
+        
+        seikakuView.layer.borderWidth = 2.0
+        tokuseiView.layer.borderWidth = 2.0
+        doryokuchiView.layer.borderWidth = 2.0
+        seikakuView.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0).cgColor
+        tokuseiView.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0).cgColor
+        doryokuchiView.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0).cgColor
+        seikakuView.layer.cornerRadius = 10
+        tokuseiView.layer.cornerRadius = 10
+        doryokuchiView.layer.cornerRadius = 10
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         titleText.text = ikuseiron.title
+        nameText.text = ikuseiron.name
+        seikakuText.text = ikuseiron.seikaku
+        tokuseiText.text = ikuseiron.tokusei
+        doryokuchiText.text = ikuseiron.doryokuchi
+        movesText.text = ikuseiron.moves
+        itemText.text = ikuseiron.item
+        ruleText.text = ikuseiron.rule
+        kousatuText.text = ikuseiron.kousatu
     }
     
     // NavigationBarを上につなげる
